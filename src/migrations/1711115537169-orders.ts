1874d2fa2b14fbe5454917a4adf0674b9be557d9
raybagas7 <raysam.gas@gmail.com>
2024-03-22 21:00:13 +0700

    feat(migrations): - add new table migration for orders table

---
+++ src/migrations/1711115537169-orders.ts
@@ -0,0 +1,82 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class Orders1711115537169 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'order_id',
+            type: 'uuid',
+            isPrimary: true,
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'book_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'customer_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'writer_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'point',
+            type: 'numeric',
+            isNullable: false,
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        columnNames: ['book_id'],
+        referencedColumnNames: ['book_id'],
+        referencedTableName: 'books',
+        onDelete: 'CASCADE',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        columnNames: ['writer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'FK_orders_book_id');
+    await queryRunner.dropForeignKey('orders', 'FK_orders_customer_id');
+    await queryRunner.dropForeignKey('orders', 'FK_orders_writer_id');
+
+    await queryRunner.dropTable('orders', true, true, true);
+  }
+}
